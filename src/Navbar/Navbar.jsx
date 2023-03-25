4f7d183178129343ab6578d9818b859df270d83b
Muhammad Hamza khan <hamza.khan@gagamuller.com>
2023-03-26 02:23:43 +0500

    index update

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import { NavLink } from 'react-router-dom'
 function Navbar() {
   return (
   <>
-  <div className="container-fluid nav_bg"> 
+  <div className="container-fluid nav_bg "> 
   <div className="row">
     <div className="col-10 mx-auto">
 	<nav className="navbar navbar-expand-lg navbar-light bg-light navbar-resize">
@@ -15,7 +15,7 @@ function Navbar() {
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse navbar-custom" id="navbarSupportedContent">
+    <div className="collapse navbar-collapse navbar-custom blur-three" id="navbarSupportedContent">
       <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
         <li className="nav-item">
           <NavLink className="nav-link" activeClassName="menu_active" 
